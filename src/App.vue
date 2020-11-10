992dc3cfefc6ea31493ca2728330e0da230d9637
tangzhengkui <zhengkuitang@163.com>
2020-11-10 17:30:16 +0800

    tzk

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" ref="box">
+    ffff
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+
+    }
+  },
+  created(){
+  },
+  methods:{
+
+  }
+}
+</script>
+<style lang="scss">
+#app{
+  width: 1015px;
+  height: 674px;
+  margin:  auto;
+  border:1px solid pink;
+}
+</style>
